f700a41866babcf145f7b77166e89931ba8a1729
brian d foy <briandfoy@pobox.com>
2022-12-29 12:39:01 -0500

    Bump version

--- lib/File/Find/Closures.pm
+++ lib/File/Find/Closures.pm
@@ -12,7 +12,7 @@ use File::Basename        qw(dirname);
 use File::Spec::Functions qw(canonpath no_upwards);
 use UNIVERSAL;
 
-our $VERSION = '1.114';
+our $VERSION = '1.115';
 
 our @EXPORT_OK   = qw(
 	find_by_created_after
